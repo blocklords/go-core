be7757375f1279a88377987c1daea02fb340242a
willow <719896508@qq.com>
2024-11-14 16:34:29 +0800

    ecdsa key

--- fn/helper.go
+++ fn/helper.go
@@ -5,7 +5,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/google/uuid"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
@@ -27,50 +26,6 @@ func Date() string {
 
 func HashFromUUID() common.Hash {
 	return common.HexToHash(hex.EncodeToString(Sha1([]byte(uuid.New().String()))))
-}
-
-type VRS struct {
-	r common.Hash `json:"r"`
-	s common.Hash `json:"s"`
-	v uint8       `json:"v"`
-}
-
-func (rsv VRS) R() common.Hash {
-	return rsv.r
-}
-
-func (rsv VRS) S() common.Hash {
-	return rsv.s
-}
-
-func (rsv VRS) V() uint8 {
-	return rsv.v
-}
-
-func (rsv VRS) JSON() map[string]interface{} {
-	return map[string]interface{}{
-		"v": rsv.v,
-		"r": rsv.r,
-		"s": rsv.s,
-	}
-}
-
-func NewVRS(sign []byte) VRS {
-	hash := common.Bytes2Hex(sign)
-	rS := hash[0:64]
-	sS := hash[64:128]
-	R := [32]byte{}
-	S := [32]byte{}
-	copy(R[:], common.FromHex(rS))
-	copy(S[:], common.FromHex(sS))
-	vS := hash[128:130]
-	vI, _ := strconv.Atoi(vS)
-
-	return VRS{
-		r: common.BytesToHash(R[:]),
-		s: common.BytesToHash(S[:]),
-		v: uint8(vI + 27),
-	}
 }
 
 func If[T any](condition bool, trueVal, falseVal T) T {
