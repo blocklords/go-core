17af732701b9e51c7dc6b78ae38353711255c73c
willow <719896508@qq.com>
2024-12-09 14:25:07 +0800

    white list

---
+++ white_list/mouse.go
@@ -0,0 +1,49 @@
+package white_list
+
+import (
+	"math/rand/v2"
+	"time"
+)
+
+var (
+	dynastyIds = []uint64{
+		891197,
+		27643,
+		244,
+		28481,
+		26527,
+		115816,
+		115610,
+		891380,
+		733108,
+		230,
+		31879,
+		792752,
+		734224,
+		91757,
+		792702,
+		32883,
+		36110,
+		792870,
+		26544,
+		122532,
+		115652,
+		115890,
+		762785,
+		38194,
+		1766,
+		734049,
+		128176,
+	}
+)
+
+func GetRandMouseId() uint64 {
+	destination := make([]uint64, len(dynastyIds))
+	copy(destination, dynastyIds)
+	rand.New(rand.NewPCG(uint64(time.Now().UnixNano()), 0))
+	rand.Shuffle(len(destination), func(i, j int) {
+		destination[i], destination[j] = destination[j], destination[i]
+	})
+
+	return destination[0]
+}
