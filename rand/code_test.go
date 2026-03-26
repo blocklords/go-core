e101bc4d24ac5d73db68fbaf62f6c0c1c6c8238a
willow <719896508@qq.com>
2026-03-26 13:49:31 +0800

    x

--- rand/code_test.go
+++ rand/code_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 // go test github.com/blocklords/go-core/rand -run TestRandCode_Make -v
 func TestRandCode_Make(t *testing.T) {
+	rc := NewRandCode(
+		//RandCodeFormat(NewFormatString(FormatStringGroup(0))),
+	)
 	for i := 0; i < 10; i++ {
-		rc := NewRandCode(
-			//RandCodeFormat(NewFormatString(FormatStringGroup(0))),
-		)
 		code := rc.Make()
 		t.Logf("code: %s", code)
 	}
